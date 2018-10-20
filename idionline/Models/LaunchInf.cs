ff9781483d2f6db5deed48e4559a1cca906e34a6
边宇琨 <bianyukun1213@outlook.com>
2018-10-20 11:15:52 +0800

    Update LaunchInf.cs

--- idionline/Models/LaunchInf.cs
+++ idionline/Models/LaunchInf.cs
@@ -9,7 +9,8 @@ namespace Idionline.Models
     public class LaunchInf
     {
         public string Text { get; set; }
-        public string DailyIdiom { get; set; }
+        public string DailyIdiomName { get; set; }
+        public int DailyIdiomId { get; set; }
         [Key]
         public long DateUT { get; set; }
     }
