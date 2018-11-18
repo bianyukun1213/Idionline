9f7363a299d3542342aacf279e5b21fde1c663dd
边宇琨 <bianyukun1213@outlook.com>
2018-11-18 18:34:19 +0800

    没啥……

--- idionline/Models/Idiom.cs
+++ idionline/Models/Idiom.cs
@@ -9,10 +9,10 @@ namespace Idionline.Models
         [BsonId]
         public ObjectId Id { get; set; }
         public string Name { get; set; }
+        public char Index { get; set; }
         public List<Definition> Definitions { get; set; }
         public string Source { get; set; }
         public string LastEditor { get; set; }
         public long UpdateTimeUT { get; set; }
-        public char Index { get; set; }
     }
 }
