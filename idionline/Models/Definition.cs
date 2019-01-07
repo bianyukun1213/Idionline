9fb08d7c19e2a027f5045889a2a620a063073684
边宇琨 <bianyukun1213@outlook.com>
2019-01-07 19:17:50 +0800

    新增示例

--- idionline/Models/Definition.cs
+++ idionline/Models/Definition.cs
@@ -6,6 +6,7 @@ namespace Idionline.Models
     {
         public string Source { get; set; }
         public string Text { get; set; }
+        public List<string> Examples { get; set; }
         public string Addition { get; set; }
         public bool IsBold { get; set; }
         public Dictionary<string, string> Links { get; set; }
