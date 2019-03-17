7a6d35fd729889a9a1c4d34de00686100f1c7f9b
边宇琨 <bianyukun1213@outlook.com>
2019-03-17 09:46:08 +0800

    1.0.41

--- idionline/obj/Debug/netcoreapp2.1/idionline.AssemblyInfo.cs
+++ idionline/obj/Debug/netcoreapp2.1/idionline.AssemblyInfo.cs
@@ -13,11 +13,11 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Idionline")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.40.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.40")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.41.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.41")]
 [assembly: System.Reflection.AssemblyProductAttribute("Idionline")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Idionline")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.40.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.41.0")]
 
 // 由 MSBuild WriteCodeFragment 类生成。
 
