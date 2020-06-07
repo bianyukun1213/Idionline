91357934d0756310f7d0ece4a9f4744c235424ba
边宇琨 <bianyukun1213@outlook.com>
2020-06-07 13:17:58 +0800

    修改错误码说明消息

--- idionline/Models/StandardReturn.cs
+++ idionline/Models/StandardReturn.cs
@@ -8,7 +8,7 @@ namespace Idionline.Models
     public class StandardReturn
     {
         public int Code { get; set; }
-        public string State { get; set; }
+        public string Msg { get; set; }
         public object Result { get; set; }
         public StandardReturn(int code = 0,object result=null)
         {
@@ -17,32 +17,32 @@ namespace Idionline.Models
                 //1：系统，2：数据库，3：第三方。
                 case 0: 
                     Code = 0;
-                    State = "成功";
+                    Msg = "成功";
                     Result = result;
                     break;
                 case -1:
                     Code = -1;
-                    State = "未知错误";
+                    Msg = "未知错误";
                     break;
                 case 20001:
                     Code = 20001;
-                    State = "未查找到数据";
+                    Msg = "未查找到数据";
                     break;
                 case 20002:
                     Code = 20002;
-                    State = "数据格式不正确，无法更新数据";
+                    Msg = "数据格式不正确，无法更新数据";
                     break;
                 case 20003:
                     Code = 20003;
-                    State = "无操作权限，无法更新数据";
+                    Msg = "无操作权限，无法更新数据";
                     break;
                 case 20004:
                     Code = 20004;
-                    State = "不允许重复提交，无法更新数据";
+                    Msg = "不允许重复提交，无法更新数据";
                     break;
                 case 30001:
                     Code = 30001;
-                    State = "第三方服务错误";
+                    Msg = "第三方服务错误";
                     break;
             }
         }
