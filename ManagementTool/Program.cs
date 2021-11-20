afd9dbb9715d794a2e7b29903aeb8c14ff6f9f8f
边宇琨 <bianyukun1213@outlook.com>
2021-11-20 18:25:16 +0800

    update

---
+++ ManagementTool/Program.cs
@@ -0,0 +1,12 @@
+﻿using Idionline.Models;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+var db = new MongoClient("mongodb://localhost:27017").GetDatabase("db_idionline");
+var idioms = db.GetCollection<Idiom>("idioms");
+var launchInfo = db.GetCollection<LaunchInfo>("launchinfo");
+var editors = db.GetCollection<Editor>("editors");
+
+var idiomAll = idioms.Find(new BsonDocument()).ToList();
+
+Console.WriteLine("Done.");
